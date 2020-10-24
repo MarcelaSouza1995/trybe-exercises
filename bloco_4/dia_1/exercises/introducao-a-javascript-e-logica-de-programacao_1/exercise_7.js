f06d4059a0b1e3ce228b6626614c8e0bbf2f9817
Marcela <marcela9908@gmail.com>
2020-10-24 18:45:02 -0300

    Adiciona exercicio 7 e arquivo exercicio 6

---
+++ bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_7.js
@@ -0,0 +1,23 @@
+let nota=5;
+
+if(nota >=90 && nota <=99){
+    console.log("A")
+}
+else if(nota>=80 && nota <=89){
+    console.log("B")
+}
+else if(nota>=70 && nota <=79){
+    console.log("C")
+}
+else if(nota>=60 && nota <=69){
+    console.log("D")
+}
+else if(nota>=50 && nota <=59){
+    console.log("E")
+}
+else if(nota <50 && nota>=1){
+    console.log("F")
+}
+else {
+    console.log("erro")
+}
