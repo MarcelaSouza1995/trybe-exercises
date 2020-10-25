a36fd12f7fb278b60f46ecb0646eb209e1cda491
Marcela <marcela9908@gmail.com>
2020-10-25 16:16:30 -0300

    correçao exercicio 11

--- bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_11.js
+++ bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_11.js
@@ -3,8 +3,6 @@ let salarioLiquido = undefined;
 let descontoINSS = undefined;
 let descontoIR = undefined;
 
-let salarioComDescontoINSS=(salarioBruto - descontoINSS);
-
 //Calculo para desconto INSS
 if (salarioBruto <= 1556.94){
     descontoINSS = (salarioBruto * 0.08);
@@ -19,21 +17,24 @@ else if (salarioBruto > 5189.82){
     descontoINSS = (570.88);
 }
 
+let salarioComDescontoINSS = (salarioBruto - descontoINSS);
+
+
 //Calculo para desconto IR
 if (salarioComDescontoINSS <= 1903.98){
     descontoIR = (0);
 }
 else if (salarioComDescontoINSS >= 1903.99 && salarioComDescontoINSS <= 2826.65){
-    descontoIR = ((salarioComDescontoINSS * 0.075) + 142.80);
+    descontoIR = ((salarioComDescontoINSS * 0.075) - 142.80);
 }
 else if (salarioComDescontoINSS >= 2826.66 && salarioComDescontoINSS <=3751.05){
-    descontoIR = ((ssalarioComDescontoINSS * 0.15) + 354.80);
+    descontoIR = ((salarioComDescontoINSS * 0.15) - 354.80);
 }
-else if (salarioComDescontoINSS >= 3751.06 && salarioComDescontoINSS<= 4664.68){
-    descontoIR = ((ssalarioComDescontoINSS * 0.225) + 636.13);
+else if (salarioComDescontoINSS >= 3751,06 && salarioComDescontoINSS<= 4664.68){
+    descontoIR = ((salarioComDescontoINSS * 0.225) - 636.13);
 }
 else if (salarioComDescontoINSS > 4664.68){
-    descontoIR = ((salarioComDescontoINSS * 0.275) + 869.36);
+    descontoIR = ((salarioComDescontoINSS * 0.275) - 869.36);
 }
 
 console.log("Desconto IR R$" + descontoIR);
