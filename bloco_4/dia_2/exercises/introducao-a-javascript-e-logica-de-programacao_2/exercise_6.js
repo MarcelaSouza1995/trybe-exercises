386777572203b74efca338e2d8f79152d8bbc550
Marcela <marcela9908@gmail.com>
2020-10-28 14:15:49 -0300

    Adicona exercicio 7

---
+++ bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_6.js
@@ -0,0 +1,13 @@
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+for (let index = 0; index < numbers.length; index ++){
+    if (numbers[index] % 2 == 1 ){
+       let numberImpar = numberImpar + 1;
+      }
+}
+
+
+    console.log(numberImpar);
---
+++ bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_7.js
@@ -0,0 +1,13 @@
+//Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 500];
+
+let minNumber = 0;
+
+for (let index = 0; index < numbers.length; index ++){
+    if (numbers[index] < minNumber) {
+        minNumber = numbers[index];
+    }
+}
+
+console.log("menor numero:" + minNumber);
