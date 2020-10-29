728d05217e7031438842ebca2e46425b0c68ce02
Marcela <marcela9908@gmail.com>
2020-10-28 23:47:49 -0300

    Adicona exercicio 8

---
+++ bloco_4/dia_4/exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise_function_1.js
@@ -0,0 +1,13 @@
+function verificacaoPalindromo (palavra){
+    for(index in palavra){
+        let palindromo = [];
+        palindromo.push = palavra[index];
+    }
+    
+}
+
+verificacaoPalindromo("arara");
+
+console.log (palavra[index]);
+
+
